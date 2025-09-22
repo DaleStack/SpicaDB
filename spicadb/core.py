cb6a34a82899d179e3802a331603d77c8000dc78
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-23 01:26:44 +0800

    collection

--- spicadb/core.py
+++ spicadb/core.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 import os
+from typing import Dict, Any
 
 class SpicaDB:
     def __init__(self, db_folder='spicadb_data'):
@@ -11,3 +12,24 @@ class SpicaDB:
         if not os.path.exists(self.db_folder):
             os.makedirs(self.db_folder)
 
+    def collection(self, name: str):
+        """ Get or create collection """
+        if name not in self.collections:
+            self.collections[name] = Collection(name, self.db_folder)
+        return self.collections[name]
+
+
+class Collection:
+    def __init__(self, name: str, db_folder: str):
+        self.name = name
+        self.filename = os.path.join(db_folder, f"{name}.json")
+        self.store = self._load_collection()
+
+    def _load_collection(self) -> Dict[str, Any]:
+        """ Load collection data to json file """
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                return json.load(file)
+        return {}
+
+        
